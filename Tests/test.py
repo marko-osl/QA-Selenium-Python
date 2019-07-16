42bbb83073b265ee7b6dd05dfc4b8e9f08b6f6fb
marko-osl <oslizlo.mar@gmail.com>
2019-07-16 17:38:46 +0200

    Setting one driver to one class test, all tests contact on homepage/contact page are made with  one opened driver

--- Tests/test.py
+++ Tests/test.py
@@ -1,24 +1,30 @@
 from Tests.ContactForm import ContactForm
 from Tests.ContactPage import ContactPage
 import Tests.OpenSite
+from Tests.OpenSite import OpenSiteWithHighlandTheme
 
 
 class test(Tests.OpenSite.OpenSiteWithHighlandTheme):
-    def startContactFormOnHomepage(self):
+
+    def openTestEnv(self):
+        driver = super().open();
+        return driver
+    def startContactFormOnHomepage(self, driver):
         print("*" * 10 + "Start startContactFormOnHomepage" + "*" * 10)
         contactForm = ContactForm()
-        contactForm.correctlyOnHomepage()
-        contactForm.incorrectlyOnHomepage()
-        contactForm.emptyForm()
+        contactForm.correctlyOnHomepage(driver)
+        contactForm.incorrectlyOnHomepage(driver)
+        contactForm.emptyForm(driver)
 
-    def startContactFormOnContactPage(self):
+    def startContactFormOnContactPage(self, driver):
         print("*" * 10 + "Start startContactFormOnContactPage" + "*" * 10)
         contactForm = ContactPage()
-        contactForm.correctlyTyping()
-        contactForm.incorrectlyTyping()
-        contactForm.emptyFormOnContactPage()
+        contactForm.correctlyTyping(driver)
+        contactForm.incorrectlyTyping(driver)
+        contactForm.emptyFormOnContactPage(driver)
 
 
-startup = test()
-startup.startContactFormOnHomepage()
-startup.startContactFormOnContactPage()
+Env = OpenSiteWithHighlandTheme()
+start = test()
+start.startContactFormOnHomepage(Env.open())
+start.startContactFormOnContactPage(Env.open())
