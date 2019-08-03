46955ce9c9365c4f179a009d7f5e0e0eabbb3437
marko-osl <oslizlo.mar@gmail.com>
2019-08-03 22:50:55 +0200

    Added variables file, change all contact form test with new variables

---
+++ Tests/Highland/Variables.py
@@ -0,0 +1,22 @@
+
+# VALUES
+firstName = "Marek"
+lastName = "Test"
+question = "How I can write the best test in Selenium & Python?"
+wrongEmail = "WrongEmail"
+
+
+# CONTACT FORM - HOMEPAGE
+firstNameOnHomepage = "first_name"
+lastNameOnHomepage = ".pl_widget-contact--name #last_name"
+emailContactFormOnHomepage = ".pl_widget-contact--email #email"
+phoneNumberContactFormOnHomepage = ".pl_widget-contact--phone #phone"
+questionContactFormOnHomepage = ".pl_widget-contact--questions #question"
+submitButtonContactFormOnHomepage = ".pl_widget-contact-column-right input[type='submit']"
+firstNameErrorOnHomepage = "/html//p[.='Your name is required.']"
+phoneNumberErrorOnHomepage = "/html//p[.='A valid phone is required.']"
+emailErrorOnHomepage = "/html//p[.='A valid email is required.']"
+questionErrorOnHomepage = "/html//textarea[@id='question']"
+
+# CONTACT FORM - CONTACT PAGE
+contactPageLinkMenu = "#menu-primary [href='\/contact']"
