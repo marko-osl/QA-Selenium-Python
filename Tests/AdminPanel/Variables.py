d8dfa7735304051a5f61d6e17c71ee7ab0fbc549
marko-osl <oslizlo.mar@gmail.com>
2019-08-12 19:13:27 +0200

    Added test login to Admin panel on every environment

---
+++ Tests/AdminPanel/Variables.py
@@ -0,0 +1,13 @@
+# VALUES
+firstName = "Marek"
+lastName = "Test"
+question = "How I can write the best test in Selenium & Python?"
+wrongEmail = "WrongEmail"
+domainInternal = "net"
+domainStaging = "xyz"
+domainProduction = "com"
+
+#LOGIN TO THE ADMIN PANEL
+emailField = ".login input[name='email']"
+passwordField = ".login input[name='password']"
+loginButton = ".login .btn"
--- Tests/Highland/OpenSite.py
+++ Tests/Highland/OpenSite.py
@@ -7,13 +7,24 @@ import random
 class OpenSiteWithHighlandTheme():
 
     baseURL = "http://qarobothighland.myrealestateplatform.xyz/"
-    def open(self):
+    def openSites(self):
         # Setting webdriver
-        screen_name = self.urlify(self.datetime_now(str(self.open.__name__))) + '.png'
+        screen_name = self.urlify(self.datetime_now(str(self.openSites.__name__))) + '.png'
         driver = webdriver.Chrome()
         try:
             driver.maximize_window()
             driver.get(self.baseURL)
+            return driver
+        except:
+            print("Awaryjne wyjście z programu, nie udało się otworzyć strony")
+            driver.save_screenshot(super().screenShotsFolder() + "/%s" % screen_name)
+            exit();
+    def openAdmin(self):
+        # Setting webdriver
+        screen_name = self.urlify(self.datetime_now(str(self.openAdmin.__name__))) + '.png'
+        driver = webdriver.Chrome()
+        try:
+            driver.maximize_window()
             return driver
         except:
             print("Awaryjne wyjście z programu, nie udało się otworzyć strony")
