443f058c4630cab37fae0b86ca912690a2c49e85
marko-osl <oslizlo.mar@gmail.com>
2019-08-31 22:35:57 +0200

    Fixing the condition on the loop.

--- Tests/AdminPanel/CRM/DeleteAllLeads.py
+++ Tests/AdminPanel/CRM/DeleteAllLeads.py
@@ -9,7 +9,7 @@ class DeleteAllLeads(OpenSite.OpenSiteWithHighlandTheme):
             print("*" * 20)
             screen_name = self.urlify(super().datetime_now(str(self.loginToTheAdminPanelOldIncApp.__name__))) + '.png'
             if (driver == 0):
-                print("Brak drivera")
+                print("Driver error")
                 exit()
             else:
                 driver.get("https://myrealestateplatform." + domain)
@@ -21,11 +21,12 @@ class DeleteAllLeads(OpenSite.OpenSiteWithHighlandTheme):
                     loginButton = driver.find_element(By.CSS_SELECTOR, v.loginButton)
                     loginButton.click()
                     time.sleep(10)
-                    if (driver.find_element_by_css_selector(".sites-container apc-site-list-item:nth-child(2) h4") is not None):
-                        print(str(self.loginToTheAdminPanelOldIncApp.__name__) + "---- OK")
-                    else:
+                    try:
+                        if (driver.find_element_by_css_selector(".sites-container apc-site-list-item:nth-child(2) h4") is not None):
+                            print(str(self.loginToTheAdminPanelOldIncApp.__name__) + "---- OK")
+                    except:
                         driver.save_screenshot(super().screenShotsFolder() + "\\%s" % screen_name)
-                        print("Wyjście awaryjne z programu")
+                        print("Emergency exit from the program")
                         exit()
 
         def enterToTheCRM(self, driver):
@@ -33,19 +34,19 @@ class DeleteAllLeads(OpenSite.OpenSiteWithHighlandTheme):
             print("*" * 20)
             screen_name = self.urlify(super().datetime_now(str(self.enterToTheCRM.__name__))) + '.png'
             if (driver == 0):
-                print("Brak drivera")
+                print("Driver error")
                 exit()
             else:
                     if (driver.find_element_by_css_selector(v.crmButton)):
                         crmButton = driver.find_element_by_css_selector(v.crmButton)
                         crmButton.click()
                         time.sleep(7)
-                        if (driver.find_element_by_css_selector(".nav--bar li:nth-of-type(2) span") is not None):
-                            print(str(self.loginToTheAdminPanelOldIncApp.__name__) + "---- OK")
-                        else:
-                            driver.save_screenshot(
-                            super().screenShotsFolder() + "\\%s" % screen_name)
-                            print("Wyjście awaryjne z programu")
+                        try:
+                            if (driver.find_element_by_css_selector(".nav--bar li:nth-of-type(2) span") is not None):
+                                print(str(self.loginToTheAdminPanelOldIncApp.__name__) + "---- OK")
+                        except:
+                            driver.save_screenshot(super().screenShotsFolder() + "\\%s" % screen_name)
+                            print("Emergency exit from the program")
                             exit()
 
         def deleteLeads(self, driver):
@@ -56,16 +57,20 @@ class DeleteAllLeads(OpenSite.OpenSiteWithHighlandTheme):
                 print("Driver error")
                 exit()
             else:
-                while(driver.find_element(By.LINK_TEXT, "Add tags") is not None ):
-                    time.sleep(1)
-                    selectAllLeads = driver.find_element_by_css_selector(v.selectAllLeadsOnSite)
-                    selectAllLeads.click()
-                    deleteButton = driver.find_element_by_css_selector(v.deleteButton)
-                    deleteButton.click()
-                    deleteConfirm = driver.find_element_by_xpath(v.deleteConfirm)
-                    deleteConfirm.click()
-                    time.sleep(2)
-                else:
-                    print("Why ? ")
-
-
+                try:
+                    try:
+                        while driver.find_element(By.CSS_SELECTOR, ".entity-count") != 0:
+                            time.sleep(1)
+                            selectAllLeads = driver.find_element_by_css_selector(v.selectAllLeadsOnSite)
+                            selectAllLeads.click()
+                            deleteButton = driver.find_element_by_css_selector(v.deleteButton)
+                            deleteButton.click()
+                            deleteConfirm = driver.find_element_by_xpath(v.deleteConfirm)
+                            deleteConfirm.click()
+                            time.sleep(2)
+                    except:
+                        print("0 Leads on your account ? ")
+                except:
+                    driver.save_screenshot(super().screenShotsFolder() + "\\%s" % screen_name)
+                    print("Emergency exit from the program")
+                    exit()
